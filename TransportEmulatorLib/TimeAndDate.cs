c10145d8eeb92ba684282344523a8def70d71ab5
Nikita Mishchanka <niklive103@gmail.com>
2016-12-13 22:08:51 +0300

    Little Steps into creating Simulation Timer, test commit

---
+++ TransportEmulatorLib/TimeAndDate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportEmulatorLib
+{
+    class TimeAndDate
+    {
+        public int _year;
+        public int _month;
+        public int _day;
+        public int _hour;
+        public int _minute;
+        public int _second;
+        public TimeAndDate()
+        {
+
+        }
+    }
+}
